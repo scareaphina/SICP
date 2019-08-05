739cd80ba4efc61820cddb5853330d278005e8dd
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-05 10:37:33 -0400

    Update 1-3_exercises.rkt

--- 1-3_exercises.rkt
+++ 1-3_exercises.rkt
@@ -1,5 +1,20 @@
 ; 1.29
+; had to look up because the math is complicated.
 
+(define (round-to-next-even x)
+  (+ x (remainder x 2)))
+
+(define (simpson f a b n)
+  (define fixed-n (round-to-next-even n))
+  (define h (/ (- b a) fixed-n))
+  (define (simpson-term k)
+    (define y (f (+ a (* k h))))
+    (if (or (= k 0) (= k fixed-n))
+        (* 1 y)
+        (if (even? k)
+            (* 2 y)
+            (* 4 y))))
+  (* (/ h 3) (sum simpson-term 0 inc fixed-n)))
 
 ; 1.30
 
