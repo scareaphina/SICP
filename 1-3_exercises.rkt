f61417377b724631ff2bb69c9b7c9deb652693e1
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-05 10:27:49 -0400

    Update 1-3_exercises.rkt

--- 1-3_exercises.rkt
+++ 1-3_exercises.rkt
@@ -1,1 +1,11 @@
+; 1.29
 
+
+; 1.30
+
+(define (sum f a next stopnumber)
+  (define (iter a result)
+    (if (> a stopnumber)
+        result
+        (iter (next a) (+ result (f a)))))
+    (iter a 0))
