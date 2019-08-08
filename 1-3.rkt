5310f23b4c761934cc73f5839ec84180434d55b2
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-08 10:14:16 -0400

    Update 1-3.rkt

--- 1-3.rkt
+++ 1-3.rkt
@@ -195,3 +195,30 @@
 (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
                       1.0
                       2.0)
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (/ x y))
+               1.0))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
