78248764f638e85e6442049e65e6738b73e4f8c4
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-12 09:06:36 -0400

    Update 1-3_exercises.rkt

--- 1-3_exercises.rkt
+++ 1-3_exercises.rkt
@@ -103,3 +103,25 @@
 (define (sqrt x)
   (fixed-point (average-damp (lambda (y) (/ x y)))
                1.0))
+
+; Newton's method
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (cube x) (* x x x))
+((deriv cube) 5)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
