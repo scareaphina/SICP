dafb8adcec740587c4bf34366ca5e907aae33ad7
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-12 10:21:53 -0400

    Update 2-1.rkt

--- 2-1.rkt
+++ 2-1.rkt
@@ -7,3 +7,29 @@
 
 (define (linear-combination a b x y)
   (add (mul a x) (mul b y)))
+
+; 2.1 introduction to data abstraction
+
+; 2.1.1 arithmetic operations for rational numbers
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mult-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
