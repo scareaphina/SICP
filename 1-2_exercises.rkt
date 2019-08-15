b37ea40766092c1ff2f08ce7addba63a54700188
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-15 14:05:15 -0400

    Update 1-2_exercises.rkt

--- 1-2_exercises.rkt
+++ 1-2_exercises.rkt
@@ -121,6 +121,24 @@
 
 ;;;;;;;;;;;;;;;;;
 
+; second attempt
+
+(define (pascal row column)
+  (if (or (= column 1) (= column row))
+          1
+          (+ (pascal (- row 1) (- column 1))
+             (pascal (- row 1) column))))
+
+(pascal 1 1)
+(pascal 2 2)
+(pascal 3 2)
+(pascal 3 3)
+(pascal 4 2)
+(pascal 5 3)
+(pascal 6 5)
+
+;;;;;;;;;;;;;;;;;
+
 ;1.16
 
 (define (fast-expt b n)
