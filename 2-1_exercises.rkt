281ac85d9524d92a27a526117ed80f89c52e81ae
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-29 10:56:30 -0400

    Update 2-1_exercises.rkt

--- 2-1_exercises.rkt
+++ 2-1_exercises.rkt
@@ -1,4 +1,5 @@
 ; 2.7
+; 2.8
 
 (define (make-interval a b)
   (cons a b))
@@ -13,6 +14,10 @@
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound x) (upper-bound y))))
 
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                 (- (upper-bound x) (upper-bound y))))
+
 (define (mul-interval x y)
   (let ((p1 (* (lower-bound x) (lower-bound y)))
         (p2 (* (lower-bound x) (upper-bound y)))
@@ -20,8 +25,3 @@
         (p4 (* (upper-bound x) (upper-bound y))))
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
-
-(define (div-interval x y)
-  (mul-interval x
-                (make-interval (/ 1.0 (upper-bound y))
-                               (/ 1.0 (lower-bound y)))))
