7b38fca9422538932b7318c56a006bfed13db174
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-11 11:02:59 -0400

    Update 2-2_exercises.rkt

--- 2-2_exercises.rkt
+++ 2-2_exercises.rkt
@@ -1,1 +1,12 @@
 #lang sicp
+
+; 2.17
+
+(define (last-pair list)
+  (if (null? list)
+      0
+      (last list)))
+
+(check-equal? (last-pair '(list 23 72 149 34)) '34)
+
+(last-pair '(list 23 72 149 34))
