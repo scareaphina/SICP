f7ee5314e6dcf793e7028b241ad729bb4877088a
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-11 11:52:10 -0400

    Update 2-2.rkt

--- 2-2.rkt
+++ 2-2.rkt
@@ -133,7 +133,23 @@
 
 (count-leaves (list x x))
 
+; mapping over trees
 
+(define (scale-tree1 tree factor)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree1 (car tree) factor)
+                    (scale-tree1 (cdr tree) factor)))))
+
+(scale-tree1 (list 1 (list 2 (list 3 4) 5) (list 6 7))
+            10)
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
 
 
 
