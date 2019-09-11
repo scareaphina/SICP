5ea99c4231a5388622c3535a438c5b592f69d840
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-11 10:59:48 -0400

    Update 2-2.rkt

--- 2-2.rkt
+++ 2-2.rkt
@@ -31,3 +31,48 @@
 (cons 10 one-through-four)
 
 (cons 5 one-through-four)
+
+; list operations
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25))
+
+(list-ref squares 3)
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+
+(length odds)
+
+(define (lengthiter items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(append squares odds)
+
+(append odds squares)
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+      
+      
+      
+      
+      
+      
+      
+      
+      
